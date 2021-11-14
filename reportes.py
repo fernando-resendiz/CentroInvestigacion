ff086b183e1af3938c55973665acb8b2d3c5069f
rez1619 <92747868+rez1619@users.noreply.github.com>
2021-11-13 20:06:16 -0600

    Created menu of reportes

--- reportes.py
+++ reportes.py
@@ -1,4 +1,30 @@
 import librerias as lb
 
 def menu_reportes():
+    while True:
+        lb.clear()
+        print("|===============================================|")
+        print("|     MENU REPORTES                             |")
+        print("|===============================================|")
+        print("|1) Lista de científicos                        |")
+        print("|2) Lista de proyectos                          |")
+        print("|3) Lista de proyectos por area                 |")
+        print("|4) Lista de cientificos por area               |")
+        print("|5) Lista de proyectos asignados a un cientifico|")
+        print("|6) Terminar                                    |")
+        print("|===============================================|")
+        op = lb.pide_entero(0,5,"Indique la opción deseada: ")
+        
+        if op == 1:
+            continue
+        elif op == 2:
+            continue
+        elif op == 3:
+            continue
+        elif op == 4:
+            continue
+        elif op == 5:
+            break
+        else:
+            lb.error('Opción no válida.')
     return
